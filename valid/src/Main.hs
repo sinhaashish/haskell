843ecdff38b2ae075f4538a33b8f46cf41c9e886
sinhaashish <ashi.sinha.87@gmail.com>
2019-09-15 17:31:34 +0530

    Adding fMap in Main()

--- valid/src/Main.hs
+++ valid/src/Main.hs
@@ -2,6 +2,13 @@ module Main where
 
 import Data.Char
 
+newtype Password =
+  Password String deriving  (Eq, Show)
+newtype Error =
+  Error String deriving  (Eq, Show)
+newtype Username =
+  Username String deriving  (Eq, Show)
+
 maxLength :: String -> Maybe String
 maxLength "" = Nothing 
 maxLength xs =
@@ -15,6 +22,20 @@ maxLengthEither xs =
   case (length xs >20 )  of
     True -> Left "Your password is more than 20 character."
     False -> Right xs
+
+passwordLength :: String -> Either Error Password
+passwordLength "" = Left (Error "Your Password is blank.")
+passwordLength password =
+  case (length password >20 )  of
+    True -> Left (Error"Your password is more than 20 character.")
+    False -> Right (Password password) 
+
+usernameLength :: String -> Either Error Username
+usernameLength username =
+  case (length username >16 )  of
+    True -> Left (Error"Your Username is more than 16 character.")
+    False -> Right (Username username) 
+  
 
 allAlpha :: String -> Maybe String
 allAlpha "" = Nothing
@@ -30,12 +51,21 @@ allAlphaEither xs =
     False -> Left "Your password is not alpha numeric."
     true -> Right xs
 
+allAlphaUserPass ::   String -> Either Error  String
+allAlphaUserPass "" = Left (Error" Your password is blank.")
+allAlphaUserPass xs =
+  case (all isAlphaNum xs ) of 
+    False -> Left (Error"Your password is not alpha numeric.")
+    true -> Right xs
+
 stripSpace :: String -> Maybe String
 stripSpace "" = Nothing
 stripSpace (x:xs) =
   case (isSpace x) of
     True -> stripSpace xs
     False -> Just (x:xs)
+
+
 
 stripSpaceEither :: String -> Either String String
 stripSpaceEither "" = Left " Your password is blank."
@@ -44,6 +74,14 @@ stripSpaceEither (x:xs) =
     True -> stripSpaceEither xs
     False -> Right (x:xs)
 
+
+stripSpaceUserPass :: String -> Either Error String
+stripSpaceUserPass "" = Left (Error" Your password is blank.")
+stripSpaceUserPass (x:xs) =
+  case (isSpace x) of
+    True -> stripSpaceUserPass xs
+    False -> Right (x:xs)
+    
 checkPassword :: String -> Maybe String
 checkPassword password =
     case (stripSpace password) of
@@ -69,8 +107,14 @@ validatePasswordEither password =
     >>= allAlphaEither
     >>= maxLengthEither  
 
+validatePass :: Password -> Either Error Password
+validatePass (Password password) =
+  stripSpaceUserPass password
+    >>= allAlphaUserPass
+    >>= passwordLength 
+
 main :: IO ()
 main = do
   putStrLn "please enter a password"
-  password <- getLine
-  print(maxLength password)
+  password <- Password <$> getLine
+  print(validatePass password)
